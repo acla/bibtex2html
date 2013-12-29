c09262248200d1c9a0e94d7954e73bbfbd56ff42
acla <acla.users.noreply.github.com@server.fake>
2013-12-29 13:38:03 +0100

    added a zip version of the wordpress plugin and a packaging script

---
+++ wp-bibtex-plugin.make-zip.sh
@@ -0,0 +1,3 @@
+# packs the contents of wp-bibtex-plugin-source into a zip file (and dereferences the symbolic link within)
+rm wp-bibtex-plugin.zip
+tar -czhf wp-bibtex-plugin.zip wp-bibtex-plugin-source/
